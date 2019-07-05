2a5f03531dcbc032cc6e21bcf928608c6e62fef1
KARNAGE <rnjvym@hi2.in>
2019-07-05 09:02:06 +0530

    fixed minor indent issue

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -359,9 +359,9 @@ async def spider(spdr):
             
         self_user = await spdr.client.get_me()
 
-         if user.id == self_user.id:
-            await spdr.edit("`Mute Error! You are not supposed to mute yourself!`")
-            return
+        if user.id == self_user.id:
+        	await spdr.edit("`Mute Error! You are not supposed to mute yourself!`")
+        	return
             
         # If the targeted user is a Sudo
         if user.id in BRAIN_CHECKER:
